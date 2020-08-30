c3cc699b148e44839e18ee3f210de6d3231dd446
宋远桥 <wanxiao.guan@ideacome.com>
2020-08-30 10:11:58 +0800

    [feature] 仅仅用 tsconfig.json 实现语法 es5

--- build/base.js
+++ build/base.js
@@ -9,13 +9,13 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 config.mode(process.env.NODE_ENV)
 // 修改 entry 配置
 config.entry('index')
-      .add(resolve('../src/index.js'))
+      .add(resolve('../src/a.ts'))
       .end()
       // 修改 output 配置
       .output
         .path(resolve('../dist'))
         .filename('[name].bundle.js');
-
+config.resolve.extensions.merge(['.ts','.js', '.jsx', '.vue', '.json'])
 config.module
     .rule('extract-css-loader')
       .test(/\.(le|c)ss$/)
@@ -30,6 +30,29 @@ config.module
       .loader('css-loader')
       .options({});
 
+config.module
+      .rule('ts-loader')
+        .test(/\.tsx?$/)
+          // .use('babel-loader')
+          //   .loader('babel-loader')
+          //   .options({
+          //     presets: ['@babel/preset-env'],
+          //     plugins: [
+          //       [
+          //         '@babel/plugin-transform-runtime',
+          //         {
+          //           corejs: 3
+          //         }
+          //       ]
+          //     ]
+          //   })
+          //   .end()
+          .use('ts-loader')
+            .loader('ts-loader')
+            .options({
+              // appendTsSuffixTo: [/\.vue$/]
+            })
+            .end()
 
 config.plugin('html').use(htmlPlugin,[{
   title:'标题党',
