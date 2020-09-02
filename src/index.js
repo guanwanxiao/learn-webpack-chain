7a3c8c7ffb72c34be2171cddce039664c9c7441d
宋远桥 <wanxiao.guan@ideacome.com>
2020-09-02 14:30:00 +0800

    [feature] 添加 babel-import

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import { Button } from 'element-ui';
 console.log('Button', Button)
+ // [
+    //   "component",
+    //   {
+    //     "libraryName": "element-ui",
+    //     "styleLibraryName": "theme-chalk"
+    //   }
+    // ]
